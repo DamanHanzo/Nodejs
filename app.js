da1aeddfac2784bf4fc71ee77bcbf49b68aa4ea0
Daman Singh <singhdamanpreet1234@gmail.com>
2017-02-06 15:39:03 -0500

    updating tutorial

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-var a = 1;
-var b = 2;
-var c = a+b;
+//function statement
 
-console.log("My first nodejs addition"+": "+c);
+function greet(h){
+    console.log("Hello Human!"+ " " + h);
+}
+greet();
+
+// functions are first-class
+
+function logGreeting(fn){
+    fn();
+}
+logGreeting(greet);
+
+//function in JS are objects. They can be passed around like variables.
