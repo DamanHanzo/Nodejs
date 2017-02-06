225659df5a160a9c20e4df2cf3e44c948ed5d3b0
Daman Singh <singhdamanpreet1234@gmail.com>
2017-02-06 15:45:20 -0500

    adding launch.json file

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ function logGreeting(fn){
 logGreeting(greet);
 
 //function in JS are objects. They can be passed around like variables.
+
+//function expression
+var greetMe = function(){
+    console.log("Hello World!");
+} 
+
+//it's still first-class
+logGreeting(greetMe);
