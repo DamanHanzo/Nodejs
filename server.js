5243c85358f23e7ab989d4543200ab8449b9f5f6
Daman Singh <singhdamanpreet1234@gmail.com>
2017-02-17 16:14:30 -0500

    nodejs server example

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html' });
+    var html = fs.readFileSync(__dirname + '/index.html');
+    res.end(html);
+
+}).listen(1337, '127.0.0.1');
